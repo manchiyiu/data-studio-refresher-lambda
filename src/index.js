869c02bb67d703216f49a73bddc0d7520021d27f
Isaac Yiu <ymcatar@gmail.com>
2019-01-12 10:34:55 +0800

    update: add data studio refresher script

--- src/index.js
+++ src/index.js
@@ -13,48 +13,70 @@ exports.handler = async (event, context, callback) => {
 };
 
 exports.run = async (browser) => {
-  // implement here
-  // this is sample
   const page = await browser.newPage();
-  await page.goto('https://www.google.co.jp',
-   {waitUntil: ['domcontentloaded', 'networkidle0']}
-  );
-  console.log((await page.content()).slice(0, 500));
 
-  await page.type('#lst-ib', 'aaaaa');
-  // avoid to timeout waitForNavigation() after click()
-  await Promise.all([
-    // avoid to
-    // 'Cannot find context with specified id undefined' for localStorage
-    page.waitForNavigation(),
-    page.click('[name=btnK]'),
-  ]);
+  // login
 
-/* screenshot
-  await page.screenshot({path: '/tmp/screenshot.png'});
-  const aws = require('aws-sdk');
-  const s3 = new aws.S3({apiVersion: '2006-03-01'});
-  const fs = require('fs');
-  const screenshot = await new Promise((resolve, reject) => {
-    fs.readFile('/tmp/screenshot.png', (err, data) => {
-      if (err) return reject(err);
-      resolve(data);
-    });
+  console.log('Logging in to Google');
+  await page.goto('https://accounts.google.com/', {
+    waitUntil: ['domcontentloaded', 'networkidle0'],
   });
-  await s3.putObject({
-    Bucket: '<bucket name>',
-    Key: 'screenshot.png',
-    Body: screenshot,
-  }).promise();
-*/
 
-  // cookie and localStorage
-  await page.setCookie({name: 'name', value: 'cookieValue'});
-  console.log(await page.cookies());
-  console.log(await page.evaluate(() => {
-    localStorage.setItem('name', 'localStorageValue');
-    return localStorage.getItem('name');
-  }));
+  console.log('Typing in username');
+  await (await page.$('input')).type(process.env.G_USERNAME);
+  await page.waitFor(500);
+  await page.keyboard.press('Enter');
+
+  console.log('Wait 3 seconds');
+  await page.waitFor(3000);
+
+  console.log('Typing in password');
+  await (await page.$('input')).type(process.env.G_PASSWORD);
+  await page.waitFor(500);
+  await page.keyboard.press('Enter');
+
+  console.log('Wait 3 seconds');
+  await page.waitFor(3000);
+
+  const title = await page.evaluate((el) => el.innerHTML, await page.$('h1'));
+  if (title.trim() == 'Verify that it\'s you') {
+    console.log('Login blocked');
+
+    // click "Confirm your recovery email"
+    await page.$eval('form', (form) => form.submit());
+
+    console.log('Wait 3 seconds');
+    await page.waitFor(3000);
+
+    await (await page.$('input')).type(process.env.RECOVERY_EMAIL);
+    await page.keyboard.press('Enter');
+
+    console.log('Wait 3 seconds');
+    await page.waitFor(3000);
+  }
+
+  console.log('Reaching dashboard');
+  await page.goto(process.env.URL);
+
+  console.log('Wait 3 seconds');
+  await page.waitFor(3000);
+
+  console.log('Wait until page is rendered');
+  await page.waitForSelector('.header-button-icon.refreshIcon');
+
+  console.log('Clicking the refresh button');
+  await page.evaluate(() => {
+    angular
+      .element('.header-button')
+      .scope()
+      .appHeaderCtrl.refreshReport();
+  });
+
+  console.log('Wait for 3 seconds');
+  await page.waitFor(3000);
+
+  console.log('Dashboard refreshed');
+
   await page.close();
   return 'done';
 };
